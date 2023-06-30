28847eddb543a12da74be4a16edba3fae2535383
FelipeAriki <felipeariki3@gmail.com>
2023-06-30 09:37:22 -0300

    Adicionar arquivos de projeto.

---
+++ SeguroAgil/Models/Banco.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+
+namespace SeguroAgil.Models
+{
+    public class Banco
+    {
+        public Banco()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+
+        }
+    }
+}
