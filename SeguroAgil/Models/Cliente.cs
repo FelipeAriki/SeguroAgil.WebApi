c6247558e9091fcbfd90070fad10602f758a1f5c
FelipeAriki <felipeariki3@gmail.com>
2023-07-04 14:51:50 -0300

    Curso Gringo, reformulando projeto corretamente.

--- SeguroAgil/Models/Cliente.cs
+++ SeguroAgil/Models/Cliente.cs
@@ -1,28 +1,48 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SeguroAgil.Models
 {
+    [BsonIgnoreExtraElements]
     public class Cliente
     {
-        public ObjectId Id { get; set; }
-        public string Nome { get; set; }
-        public string Sobrenome { get; set; }
-        public Date Idade { get; set; }
-        public string Email { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("nome")]
+        public string Nome { get; set; } = string.Empty;
+
+        [BsonElement("sobrenome")]
+        public string Sobrenome { get; set; } = string.Empty;
+
+        [BsonElement("idade")]
+        public Date? Idade { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; } = string.Empty;
+
 
         //Endereço
-        public string Rua { get; set; }
-        public string Bairro { get; set; }
-        public string Cep { get; set; }
-        public string Logradouro { get; set; }
-        public string Complemento { get; set; }
+        [BsonElement("rua")]
+        public string Rua { get; set; } = string.Empty;
+
+        [BsonElement("bairro")]
+        public string Bairro { get; set; } = string.Empty;
+
+        [BsonElement("cep")]
+        public string Cep { get; set; } = string.Empty;
+
+        [BsonElement("logradouro")]
+        public string Logradouro { get; set; } = string.Empty;
+
+        [BsonElement("complemento")]
+        public string Complemento { get; set; } = string.Empty;
+
+        [BsonElement("numero")]
         public int Numero { get; set; }
 
-        public Cliente()
-        {
-
-        }
     }
 }
